59afe61158f64a1ac2709b89c6ce0ddb8e7decce
Mayank Ranjan <Mayank.Ranjan@philips.com>
2020-09-05 18:16:30 +0530

    Update anagram.cs

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -5,12 +5,52 @@ namespace Anagram
 {
     public class AnagramSelector
     {
+        public bool CheckAnagram(List<String> list1,List<String> list2)
+        {
+            for(int i=0;i<list1.Count;i++)
+            {
+                if(!list1[i].Equals(list2[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
         public bool WordPairIsAnagram(string word1, string word2) {
-            //Insert the correct implementation here
+            if (word1.Length == word2.Length)
+            {
+                List<String> list = new List<String>();
+                List<String> lists = new List<String>();
+                for (int i = 0; i < word1.Length; i++)
+                {
+                    String alphabet = word1.Substring(i,1);
+                    String alphabets = word2.Substring(i,1);
+                    list.Add(alphabet);
+                    lists.Add(alphabets);
+                }
+                list.Sort();lists.Sort();
+                return CheckAnagram(list, lists);
+                
+            }
             return false;
         }
         public List<string> SelectAnagrams(string word, List<string> candidates) {
-            //Insert the correct implementation here
+
+            List<string> NotAnagaram = new List<string>();
+
+            for(int i=0;i<candidates.Count;i++)
+            {
+                if(!WordPairIsAnagram(word,candidates[i]))
+                {
+                    NotAnagaram.Add(candidates[i]);
+                }
+            }
+            
+            for(int i=0;i<NotAnagaram.Count;i++)
+            {
+                candidates.Remove(NotAnagaram[i]);
+            }
+
             return candidates;
         }
     }
